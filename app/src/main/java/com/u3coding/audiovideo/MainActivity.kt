ce22c794e03cb926731610c57adc7d36d0abc174
u3shadow <u3shadow@gmail.com>
2020-08-11 17:18:07 +0800

    添加主页跳转

--- app/src/main/java/com/u3coding/audiovideo/MainActivity.kt
+++ app/src/main/java/com/u3coding/audiovideo/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.u3coding.audiovideo
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import com.u3coding.audiovideo.showpic.ShowPicActivity
+import kotlinx.android.synthetic.main.main_activity_layout.*
 
 class MainActivity :AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity_layout)
     }
+    fun onClick(view: View){
+        var mIntent:Intent? = null
+        when(view.id){
+            R.id.goPic ->{
+                mIntent = Intent(this,ShowPicActivity::class.java)
+            }
+        }
+        startActivity(mIntent)
+    }
+
 }
