3c7d48074cb75b1d38b94cc4a7a6154dce69757d
xiaolei <xiaolei@travelsky.com>
2020-08-26 13:28:07 +0800

    AAC硬编，硬解

--- app/src/main/java/com/u3coding/audiovideo/MainActivity.kt
+++ app/src/main/java/com/u3coding/audiovideo/MainActivity.kt
@@ -2,25 +2,34 @@ package com.u3coding.audiovideo
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.u3coding.audiovideo.opengl.OpenGLES20Activity
 import com.u3coding.audiovideo.showpic.ShowPicActivity
-import kotlinx.android.synthetic.main.main_activity_layout.*
 
-class MainActivity :AppCompatActivity(){
+
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity_layout)
+        Log.e("context1", "getApplication in Activity: " + getApplication().javaClass.getName())
+        Log.e(
+            "context1",
+            "getApplicationContext in Activity: " + getApplicationContext().javaClass.getName()
+        )
+        Log.e("context1", "getBaseContext in Activity: " + getBaseContext().javaClass.getName())
+
     }
-    fun onClick(view: View){
-        var mIntent:Intent? = null
-        when(view.id){
-            R.id.goPic ->{
-                mIntent = Intent(this,ShowPicActivity::class.java)
+
+    fun onClick(view: View) {
+        var mIntent: Intent? = null
+        when (view.id) {
+            R.id.goPic -> {
+                mIntent = Intent(this, ShowPicActivity::class.java)
             }
-            R.id.goOpenGl ->{
-                mIntent = Intent(this,OpenGLES20Activity::class.java)
+            R.id.goOpenGl -> {
+                mIntent = Intent(this, OpenGLES20Activity::class.java)
             }
         }
         startActivity(mIntent)
---
+++ app/src/main/java/com/u3coding/audiovideo/aac/BaseEncoder.java
@@ -0,0 +1,5 @@
+package com.u3coding.audiovideo.aac;
+
+public abstract class BaseEncoder {
+   abstract public boolean encode(String path, String name);
+}
